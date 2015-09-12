4e58cfa92eedac7c01cd14cd7619b5e8d1d104f1
Chang Kon Han <chang_kon@hotmail.com>
2015-09-13 01:02:35 +1200

    test ui-router

--- app/app.js
+++ app/app.js
@@ -1,18 +1,28 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
 
-app.controller('TabCtrl', ['$scope', '$http', function($scope, $http) {
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
 
-	$http.get('https://api.edmodo.com/assignments?access_token=12e7eaf1625004b7341b6d681fa3a7c1c551b5300cf7f7f3a02010e99c84695d').
-	then(function(response) {
-		//success
-		$scope.data = response.data;
-		console.log(response);
-	}, function(response) {
-		//failure
-		console.log('fail');
+	$stateProvider.state('home', {
+		url: '/',
+		templateUrl: 'app/views/home.html'
 	});
-}]);
 
-app.controller('ContentCtrl', ['$scope', function($scope) {
-	$scope.greeting = 'lol';
-}]);
+});
+
+// app.controller('TabCtrl', ['$scope', '$http', function($scope, $http) {
+
+// 	$http.get('https://api.edmodo.com/assignments?access_token=12e7eaf1625004b7341b6d681fa3a7c1c551b5300cf7f7f3a02010e99c84695d').
+// 	then(function(response) {
+// 		//success
+// 		$scope.data = response.data;
+// 		console.log(response);
+// 	}, function(response) {
+// 		//failure
+// 		console.log('fail');
+// 	});
+// }]);
+
+// app.controller('ContentCtrl', ['$scope', function($scope) {
+// 	$scope.greeting = 'lol';
+// }]);
