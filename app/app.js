865ca9198c08a7f4c180985de11e8fdb8a14d5ed
Chang Kon Han <chang_kon@hotmail.com>
2015-09-13 01:11:42 +1200

    Separated controllers

--- app/app.js
+++ app/app.js
@@ -5,24 +5,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider.state('home', {
 		url: '/',
-		templateUrl: 'app/views/home.html'
+		templateUrl: 'app/views/home.html',
+		controller: 'app/controllers/home.js'
 	});
 
 });
-
-// app.controller('TabCtrl', ['$scope', '$http', function($scope, $http) {
-
-// 	$http.get('https://api.edmodo.com/assignments?access_token=12e7eaf1625004b7341b6d681fa3a7c1c551b5300cf7f7f3a02010e99c84695d').
-// 	then(function(response) {
-// 		//success
-// 		$scope.data = response.data;
-// 		console.log(response);
-// 	}, function(response) {
-// 		//failure
-// 		console.log('fail');
-// 	});
-// }]);
-
-// app.controller('ContentCtrl', ['$scope', function($scope) {
-// 	$scope.greeting = 'lol';
-// }]);
