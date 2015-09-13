0bf75b2b61a8ba02f278cf588554aeb78e1f72a0
Chang Kon Han <chang_kon@hotmail.com>
2015-09-14 01:24:13 +1200

    Added client side adding assignment

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -8,10 +8,44 @@ app.query = {
 };
 
 app.controller('TabCtrl', ['$scope', '$http', function($scope, $http) {
+	var counter = -1;
 
-	$scope.assignmentTab = function() {
-
+	// initialize
+	$scope.newAssignment = {
+		title: "",
+		due_at: "",
+		description: "",
+		id: counter,
+		creator: {
+			id: counter
+		}
 	};
+
+	$scope.addAssignment = function() {
+		
+		if ($scope.newAssignment.title == '' || $scope.newAssignment.due_at == '' || $scope.newAssignment.description == '') {
+			return;
+		}
+
+		// clone object
+		var copy = {
+			title: $scope.newAssignment.title,
+			due_at: $scope.newAssignment.due_at,
+			description: $scope.newAssignment.description,
+			id: $scope.newAssignment.id,
+			creator: $scope.newAssignment.creator
+		};
+
+		$scope.data.push(copy);
+
+		// empty
+		$scope.newAssignment.title = '';
+		$scope.newAssignment.due_at = '';
+		$scope.newAssignment.description = '';
+
+		counter--;
+		$scope.newAssignment.id = counter;
+	}
 
 	$http.get(app.query.assignment).
 	then(function(response) {
@@ -30,6 +64,10 @@ app.controller('ContentCtrl', ['$scope', '$http', '$location', function($scope, 
 app.controller('DetailCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
 	var id = $stateParams.id;
 	var creator_id = $stateParams.creator_id;
+
+	// determines if object is mock object
+	var mockObject = id < 0 && creator_id < 0 ? true : false;
+
 	$scope.tabList = ['Assignment', 'Submissions'];
 	$scope.showSubmission = false;
 	$scope.submissionIndex = -1;
@@ -62,22 +100,66 @@ app.controller('DetailCtrl', ['$scope', '$http', '$stateParams', function($scope
 		return obj.id == id;
 	};
 
-	// query assignment
-	$http.get(app.query.assignment).
-	then(function(response) {
-		var match = response.data.filter(findAssignment)[0];
-		$scope.title = match.title;
-		$scope.due_at = match.due_at;
-		$scope.description = match.description;
-	}, function(response) {
-		console.log('fail');
-	});
+	if (mockObject) {
+		// add mock data
+		$scope.title = "Placeholder title";
+		$scope.due_at = new Date("2015-09-25");
+		$scope.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nisi finibus, rhoncus nulla ac, lobortis magna. Vestibulum a nunc porttitor, pharetra diam quis, egestas tellus. Donec rutrum ipsum at lorem ultrices consequat. Suspendisse auctor erat at augue tempor, nec commodo turpis elementum. In vehicula, urna ac molestie dignissim, arcu nunc hendrerit nunc, quis ullamcorper lectus elit ac risus. Suspendisse lacinia bibendum turpis.";
+		$scope.submissions = [
+			{
+				creator: {
+					first_name: "Henry",
+					last_name: "Williams"
+				},
+				submitted_at: new Date('2015-08-30'),
+				content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nisi finibus, rhoncus nulla ac, lobortis magna. Vestibulum a nunc porttitor, pharetra diam quis, egestas tellus."
+			},
+			{
+				creator: {
+					first_name: "John",
+					last_name: "Hill"
+				},
+				submitted_at: new Date('2015-08-30'),
+				content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nisi finibus, rhoncus nulla ac, lobortis magna. Vestibulum a nunc porttitor, pharetra diam quis, egestas tellus."
+			},
+			{
+				creator: {
+					first_name: "Lucas",
+					last_name: "Watson"
+				},
+				submitted_at: new Date('2015-08-30'),
+				content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nisi finibus, rhoncus nulla ac, lobortis magna. Vestibulum a nunc porttitor, pharetra diam quis, egestas tellus."
+			},
+			{
+				creator: {
+					first_name: "Jenny",
+					last_name: "Liu"
+				},
+				submitted_at: new Date('2015-08-30'),
+				content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nisi finibus, rhoncus nulla ac, lobortis magna. Vestibulum a nunc porttitor, pharetra diam quis, egestas tellus."
+			},
+		];
+	} else {
+		// load data
 
-	//query assignment submissions
-	$http.get(app.query.assignment_submission1 + id + app.query.assignment_submission2 + creator_id + app.query.assignment_submission3).
-	then(function(response) {
-		$scope.submissions = response.data;
-	}, function(response) {
-		console.log('fail');
-	});
+		// query assignment
+		$http.get(app.query.assignment).
+		then(function(response) {
+			var match = response.data.filter(findAssignment)[0];
+			$scope.title = match.title;
+			$scope.due_at = match.due_at;
+			$scope.description = match.description;
+		}, function(response) {
+			console.log('fail');
+		});
+
+		//query assignment submissions
+		$http.get(app.query.assignment_submission1 + id + app.query.assignment_submission2 + creator_id + app.query.assignment_submission3).
+		then(function(response) {
+			$scope.submissions = response.data;
+		}, function(response) {
+			console.log('fail');
+		});
+	}
+
 }]);
